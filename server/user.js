de7c8dffa456976bf65d8a1f36b25e22396ab37c
rxyshww <97021898@qq.com>
2018-04-26 00:14:58 +0800

    socket.io联调

--- server/user.js
+++ server/user.js
@@ -3,6 +3,7 @@ const utils = require('utility')
 const Router = express.Router()
 const models = require('./model')
 const User = models.getModel('user')
+const Chat = models.getModel('chat')
 
 const _filter = {'pwd': 0, '__v': 0}
 
@@ -15,6 +16,14 @@ Router.get('/list', (req, res) => {
     }
     User.find(typeData, _filter, (err, doc) => {
         return res.json({code: 0, data: doc})
+    })
+})
+Router.get('/getmsglist', (req, res) => {
+    const user = req.cookies.user
+    Chat.find({}, (err, doc) => {
+        if (!err) {
+            return res.json({code: 0, msgs: doc})
+        }
     })
 })
 Router.post('/update', (req, res) => {
--- src/component/Chat/Chat.js
+++ src/component/Chat/Chat.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react'
+import { List, InputItem } from 'antd-mobile'
+import io from 'socket.io-client'
+import { connect } from 'react-redux'
+import {getMsgList} from "../../redux/chat.redux";
+const socket = io('ws://localhost:9093')
 
+@connect(
+    state=>state,
+    { getMsgList }
+)
 class Chat extends Component {
+    constructor() {
+        super()
+        this.state = {
+            text: "",
+            msg: []
+        }
+    }
+    componentDidMount() {
+        this.props.getMsgList()
+        /*socket.on('recvmsg', (data) => {
+            this.setState({
+                msg: [...this.state.msg, data.text]
+            })
+        })*/
+    }
+    handleSubmit() {
+        socket.emit('sendmsg', {text: this.state.text})
+        this.setState({text: ""})
+    }
     render() {
         return (
-            <h2>chat with user: {this.props.match.params.user}</h2>
+            <div>
+                {this.state.msg.map(v => (
+                    <p key={v}>{v}</p>
+                ))}
+                <div className="stick-footer">
+                    <List>
+                        <InputItem
+                            placeholder="请输入"
+                            value={this.state.text}
+                            onChange={v=>{
+                                this.setState({text: v})
+                            }}
+                            extra={<span onClick={()=>this.handleSubmit()}>发送</span>}
+                        />
+                    </List>
+                </div>
+            </div>
         )
     }
 }
